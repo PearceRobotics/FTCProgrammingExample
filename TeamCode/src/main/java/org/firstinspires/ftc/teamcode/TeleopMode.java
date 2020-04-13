510e6ddb0fc6d0822562df9b24e58ead6c2fe818
Eliza Barnett <elizacatbarnett@gmail.com>
2020-04-13 17:25:29 -0500

    Basic Drive Code Added

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleopMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleopMode.java
@@ -1,9 +1,29 @@
 package org.firstinspires.ftc.teamcode;
 
+
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.firstinspires.ftc.teamcode.subsystems.Drive;
 
 @TeleOp(name = "Teleop", group = "b")
 public class TeleopMode extends LinearOpMode {
 
+    private DcMotor driveMotorLeft;
+    private DcMotor driveMotorRight;
+
+    @Override
+    public void runOpMode(){
+
+        driveMotorLeft = hardwareMap.dcMotor.get("driveMotorLeft");
+        driveMotorRight = hardwareMap.dcMotor.get("driveMotorRight");
+
+        Drive drive = new Drive(driveMotorLeft, driveMotorRight);
+
+        waitForStart();
+        while (opModeIsActive()) {
+            drive.arcadeDrive(-gamepad1.left_stick_y, gamepad1.right_stick_x);
+        }
+    }
 }
